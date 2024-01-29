ea307a35f274f4a65308530e6806e01684d81154
Lalit <choudharylalit182@gmail.com>
2024-01-29 14:37:34 +0530

    initialize state and complete biolerplate

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,27 @@
 
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/LoginPage/Login';
+import Home from './pages/HomePage/Home';
+import Profile from './pages/ProfilePage/Profile';
+
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+// import { themeSettings } from "./theme";
+
+
 function App() {
+  const mode = useSelector((state) => state.mode);
+
   return (
-    <div className="App">
-      this is socail media app
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />}/>
+        <Route path='/home' element={<Home />}/>
+        <Route path='/profile/:userId' element={<Profile />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
